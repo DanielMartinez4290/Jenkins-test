989ae63b27164cd1197f60a7fc140c03c2271450
Daniel Martinez <dmartinez4290@gmail.com>
2016-06-11 23:04:47 -0500

    added to path

--- tests/ValidatePasswordTest.php
+++ tests/ValidatePasswordTest.php
@@ -1,5 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require dirname(__FILE__).'/../src/ValidatePassword.php';
+
 class ValidatePasswordTest extends PHPUnit_Framework_TestCase{
 
 	public function testValidLength(){
