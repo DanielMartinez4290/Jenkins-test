69c120d51136b959d3e5e369474237cf319da7a4
Daniel Martinez <dmartinez4290@gmail.com>
2016-06-13 01:00:12 -0500

    added validate email test

---
+++ tests/ValidateEmailTest.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require dirname(__FILE__).'/../src/ValidateEmail.php';
+
+class ValidateEmailTest extends PHPUnit_Framework_TestCase{
+
+	
+}
